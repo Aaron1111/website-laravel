563e50e5e9ddfb43bde7250522f8d117e7d52629
romarios1987 <romios047@gmail.com>
2018-04-11 19:16:43 +0300

    styles for layout(navbar)

--- public/js/app.js
+++ public/js/app.js
@@ -984,6 +984,12 @@ Vue.use(__WEBPACK_IMPORTED_MODULE_0_buefy___default.a);
 
 // Vue.component('example', require('./components/Example.vue'));
 
+var app = new Vue({
+    el: '#app',
+    data: {}
+
+});
+
 /***/ }),
 /* 11 */
 /***/ (function(module, exports, __webpack_require__) {
