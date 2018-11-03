f1e4737edbae96465b0c0234e00210aabfa6dcb3
romarios1987 <romios047@gmail.com>
2018-11-03 23:56:23 +0200

    fixed autocomplete in PHPstorm

--- config/app.php
+++ config/app.php
@@ -173,6 +173,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
     ],
 
     /*
